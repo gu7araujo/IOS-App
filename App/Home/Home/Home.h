3593548077d5ad65ccc66ce17f11a6b6ea91b2cc
Gustavo Araujo Santos <gustavosantos@ciandt.com>
2022-12-13 15:53:20 -0300

    Create modules and linked

---
+++ App/Home/Home/Home.h
@@ -0,0 +1,18 @@
+//
+//  Home.h
+//  Home
+//
+//  Created by Gustavo Araujo Santos on 13/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Home.
+FOUNDATION_EXPORT double HomeVersionNumber;
+
+//! Project version string for Home.
+FOUNDATION_EXPORT const unsigned char HomeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Home/PublicHeader.h>
+
+
