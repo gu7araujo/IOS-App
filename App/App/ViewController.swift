c84543e90ea00eccbf36254f06eb6e1be1016ec2
Gustavo Araujo Santos <gustavosantos@ciandt.com>
2022-12-13 11:18:54 -0300

    Add initial app

---
+++ App/App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by Gustavo Araujo Santos on 13/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
